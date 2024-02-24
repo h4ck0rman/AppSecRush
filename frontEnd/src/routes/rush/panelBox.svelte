504bcfa8213292be616b399cf762240ee1e56a57
h4ck0rman <h4ck0rman@gmail.com>
2024-02-24 04:57:42 -0500

    cleaned up the logos

--- frontEnd/src/routes/rush/panelBox.svelte
+++ frontEnd/src/routes/rush/panelBox.svelte
@@ -1,7 +1,8 @@
 <script>
     import PuzzleResult from "./puzzleResult.svelte";
 
-    let profile = "h4ck0rLogo.png"
+    const profile = "h4ck0rLogo.png";
+
 
     let rushActive = false;
 
@@ -26,7 +27,7 @@
 
 
 <div class="flex flex-col space-y-10 items-center py-10 bg-zinc-900 h-full">
-    <img src={profile} alt="profile of user" class="w-12"/>
+    <img src={profile} alt="profile of user" class="w-24"/>
 
     {#if !rushActive}
 
@@ -34,7 +35,7 @@
     
     {:else}
 
-    <form class="max-w-sm mx-auto">
+    <form>
         <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Vulnerable Code Line Number</label>
         <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" />
         <button on:click={solvePuzzle} class="bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 my-2 px-4 border border-white hover:border-transparent rounded">Submit</button>
--- frontEnd/src/routes/rush/puzzleResult.svelte
+++ frontEnd/src/routes/rush/puzzleResult.svelte
@@ -1,21 +1,16 @@
 <script>
     export let results;
+    const correct = "logos/green_tick.png";
+    const wrong = "logos/red_cross.png";
 
-    async function emote(result) {
-        if (result) {
-            res = "✅"
-        } else {
-            res = "❎"
-        }
-        return res;
-    }
+
 </script>
 
 <div class="container flex flex-row w-full items-start flex-wrap px-10">
     {#each results as result}
-        <div class="flex flex-col items-center px-2">
-            <p class="text-green-900">{result.result ? "✅" : "❎"}</p>
-            <p class="text-white">{result.num}</p>
+        <div class="flex flex-col items-center px-2 py-2">
+            <img src={result.result ? correct : wrong} alt="result logo" class="w-6"/>
+            <p class="text-white font-bold">{result.num}</p>
             
         </div>
     {/each}
