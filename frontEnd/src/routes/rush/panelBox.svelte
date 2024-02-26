77028afc1951384953b28b1b41b1e61311616f9d
h4ck0rman <h4ck0rman@gmail.com>
2024-02-26 07:42:13 -0500

    formatting x2

--- frontEnd/src/routes/rush/panelBox.svelte
+++ frontEnd/src/routes/rush/panelBox.svelte
@@ -40,16 +40,14 @@
     
 </script>
 
-<div class="static ...">
-    
-    
-  </div>
+
 <div class='flex flex-row w-screen h-screen rounded-lg'>
     <div class="absolute top-0 left-0 py-10 px-10">
         <a href="/">
             <p class="text-3xl text-white font-bold">{'←'}</p>
         </a>
-      </div>
+    </div>
+
     <span class='basis-1/4'>
         <div class="container flex flex-col space-y-10 py-10 bg-zinc-900 h-full">
             <img src={profile} alt="profile of user" class="w-24 py-10 mx-auto"/>
@@ -60,7 +58,7 @@
             {:else}
             <div>
                 <form class="px-10 py-10 mx-auto flex flex-col">
-                    <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Vulnerable Code Line Number</label>
+                    <label for="number-input" class="block mb-2 text-gray-900 dark:text-white font-bold text-sm uppercase">Vulnerable Line Number</label>
                     <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" />
                     <button on:click={solvePuzzle} class="bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 my-2 px-4 border border-white hover:border-transparent rounded">Submit</button>
                 </form>
