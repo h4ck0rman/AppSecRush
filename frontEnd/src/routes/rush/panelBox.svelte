56958fffe05ccfc22b7e2f6b3f64eae2140cb4d7
h4ck0rman <h4ck0rman@gmail.com>
2024-02-26 07:27:50 -0500

    added a home redirect arrow on panel box

--- frontEnd/src/routes/rush/panelBox.svelte
+++ frontEnd/src/routes/rush/panelBox.svelte
@@ -29,7 +29,7 @@
 
     const nextPuzzle = () => {
         // get the next puzzle from backend api
-        
+
         // reflect the next puzzle in the monaco editor
         codeUpdate(code, language);
     }
@@ -37,7 +37,16 @@
     
 </script>
 
+<div class="static ...">
+    
+    
+  </div>
 <div class='flex flex-row w-screen h-screen rounded-lg'>
+    <div class="absolute top-0 left-0 py-10 px-10">
+        <a href="/">
+            <p class="text-3xl text-white font-bold">{'←'}</p>
+        </a>
+      </div>
     <span class='basis-1/4'>
         <div class="container flex flex-col space-y-10 py-10 bg-zinc-900 h-full">
             <img src={profile} alt="profile of user" class="w-24 py-10 mx-auto"/>
@@ -47,7 +56,7 @@
             
             {:else}
             <div>
-                <form class="px-10 mx-auto flex flex-col">
+                <form class="px-10 py-10 mx-auto flex flex-col">
                     <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Vulnerable Code Line Number</label>
                     <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" />
                     <button on:click={solvePuzzle} class="bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 my-2 px-4 border border-white hover:border-transparent rounded">Submit</button>
